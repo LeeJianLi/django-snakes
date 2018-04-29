40121d2e6d0fbeb7823c4d4b0fb1269a3acbf4c6
lee <kairiarcher@gmail.com>
2018-04-29 16:22:55 -0700

    allow host

--- Games/Games/settings.py
+++ Games/Games/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2c5-g*o#7gmajg4_dl63xjs&c*zi4^&c8elwniats8sti4m*pv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Projectalpha.us-west-2.elasticbeanstalk.com','127.0.0.1']
 
 
 # Application definition
